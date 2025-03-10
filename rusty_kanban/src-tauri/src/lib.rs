84fb4bbc1ac35e6db0420e4423dd9ab0dab0510e
cliffy <hugodip@orange.fr>
2025-03-10 22:28:56 +0100

    style: format code for better readability in Rust commands

--- rusty_kanban/src-tauri/src/lib.rs
+++ rusty_kanban/src-tauri/src/lib.rs
@@ -1,8 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn submit_task(name: &str){
+fn submit_task(name: &str) {
     println!("I was invoked from JavaScript! the task is {}", name);
 }
+
 #[tauri::command]
 fn update_task(id: &str, text: String) {
     println!("Updating task {} with new text: {}", id, text);
@@ -19,7 +20,11 @@ fn move_task(id: &str, column: &str) {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![submit_task, update_task, move_task])
+        .invoke_handler(tauri::generate_handler![
+            submit_task,
+            update_task,
+            move_task
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
